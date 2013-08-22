b48c479c87b51a04066c2e2b976387900860ca45
Jon Williams <jonathan.williams@gmail.com>
2013-08-21 21:10:42 -0400

    #6 general cleanup and addition of pf rules

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 . /root/.profile
+. /etc/rc.conf
 
 HOSTNAME=$1
 hostname -s $HOSTNAME > /etc/myname
@@ -9,16 +10,18 @@ pkg_add polipo
 pkg_add w3m-0.5.3p1
 #pkg_add chromium
 
-#configure polipo
+echo "Configuring sshd"
+echo X11Forwarding yes >> /etc/ssh/sshd_config
+/etc/rc.d/sshd restart
+
+echo Configuring polipo and tor
 echo "socksParentProxy = \"localhost:9050\"
 socksProxyType = socks5" >> /etc/polipo/config
-
 echo "/etc/rc.d/tor start" | tee -a /etc/rc.local | sh
 echo "/etc/rc.d/polipo start" | tee -a /etc/rc.local | sh
 
+echo "Setup user account"
 proxy=http://127.0.0.1:8123
-
-#setup ~vagrant
 mkdir ~vagrant/.w3m
 echo "http_proxy $proxy
 https_proxy $proxy
@@ -27,9 +30,19 @@ use_proxy 1" > ~vagrant/.w3m/config
 echo "
 export http_proxy=$proxy
 export https_proxy=$proxy
+export all_proxy=$proxy
 export ftp_proxy=$proxy
 " > ~vagrant/.profile
 chown -R vagrant:vagrant ~vagrant
 
+echo Setting up packet filter rule
+echo 'block out on ! lo0 proto { tcp, udp } user vagrant' >> /etc/pf.conf
+pfctl -f /etc/pf.conf
+
 # disable sudo
 # mv /etc/sudoers{,.bak}
+
+echo Writing MOTD
+. ~vagrant/.profile
+lynx -width=1000 -dump -nolist http://check.torproject.org/ | sed 's/  */ /g' | egrep '[:alpha:]'| head -n 4 | tail -n 3 >> /etc/motd
+echo >> /etc/motd
